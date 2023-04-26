66818e04f21708dc4638cf2f7c4c9df01ec869b8
Pablo Deymonnaz <deymonnaz@gmail.com>
2023-04-26 16:29:31 -0300

    API scaffolding

---
+++ src/lib.rs
@@ -0,0 +1,208 @@
+pub mod commitments;
+pub mod math;
+
+use std::marker;
+
+#[allow(non_camel_case_types)]
+#[repr(C)]
+pub enum C_KZG_RET {
+    /// Success
+    C_KZG_OK = 0,
+    /// The supplied data is invalid in some way
+    C_KZG_BADARGS,
+    /// Internal error - this should never occur
+    C_KZG_ERROR,
+    /// Could not allocate memory
+    C_KZG_MALLOC,
+}
+
+/** The number of bytes in a KZG commitment. */
+pub const BYTES_PER_COMMITMENT: usize = 48;
+
+/** The number of bytes in a KZG proof. */
+pub const BYTES_PER_PROOF: usize = 48;
+
+/** The number of bytes in a BLS scalar field element. */
+pub const BYTES_PER_FIELD_ELEMENT: usize = 32;
+
+pub const FIELD_ELEMENTS_PER_BLOB: usize = 4096;
+
+/** The number of bytes in a blob. */
+pub const BYTES_PER_BLOB: usize = FIELD_ELEMENTS_PER_BLOB * BYTES_PER_FIELD_ELEMENT;
+
+pub type Bytes32 = [u8; 32];
+pub type Bytes48 = [u8; 48];
+pub type KZGCommitment = [u8; 48];
+pub type KZGProof = [u8; 48];
+pub type Blob = [u8; BYTES_PER_BLOB];
+
+#[allow(non_camel_case_types)]
+pub type limb_t = u64;
+
+#[allow(non_camel_case_types)]
+#[repr(C)]
+pub struct blst_fr {
+    l: [limb_t; 256 / 8 / core::mem::size_of::<limb_t>()],
+}
+
+#[allow(non_camel_case_types)]
+#[repr(C)]
+pub struct blst_fp {
+    l: [limb_t; 384 / 8 / core::mem::size_of::<limb_t>()],
+}
+
+#[allow(non_camel_case_types)]
+#[repr(C)]
+pub struct blst_p1 {
+    pub x: blst_fp,
+    pub y: blst_fp,
+    pub z: blst_fp,
+}
+
+#[allow(non_camel_case_types)]
+#[repr(C)]
+pub struct blst_p1_affine {
+    pub x: blst_fp,
+    pub y: blst_fp,
+}
+
+/* 0 is "real" part, 1 is "imaginary" */
+#[allow(non_camel_case_types)]
+#[repr(C)]
+pub struct blst_fp2 {
+    fp: [blst_fp; 2],
+}
+
+#[allow(non_camel_case_types)]
+pub type g1_t = blst_p1;
+/**< Internal G1 group element type. */
+#[allow(non_camel_case_types)]
+pub type g2_t = blst_p2;
+/**< Internal G2 group element type. */
+#[allow(non_camel_case_types)]
+pub type fr_t = blst_fr;
+/**< Internal Fr field element type. */
+
+#[allow(non_camel_case_types)]
+#[repr(C)]
+pub struct blst_p2 {
+    pub x: blst_fp2,
+    pub y: blst_fp2,
+    pub z: blst_fp2,
+}
+
+#[allow(non_camel_case_types)]
+#[repr(C)]
+pub struct blst_p2_affine {
+    pub x: blst_fp2,
+    pub y: blst_fp2,
+}
+
+//typedef struct { limb_t l[256/8/sizeof(limb_t)]; } blst_fr;
+//typedef struct { limb_t l[384/8/sizeof(limb_t)]; } blst_fp;
+//typedef struct { blst_fp x, y, z; } blst_p1;
+//typedef struct { blst_fp x, y; } blst_p1_affine;
+
+#[allow(non_camel_case_types)]
+#[repr(C)]
+/// Stores the setup and parameters needed for performing FFTs.
+pub struct FFTSettings<'a> {
+    /** The maximum size of FFT these settings support, a power of 2. */
+    pub max_width: u64,
+    /**
+     * We fix a given primitive roots of unity w of order `max_width` via
+     * `SCALE2_ROOT_OF_UNITY`. Then `expanded_roots_of_unity[i]` == w^i and
+     * `reverse_roots_of_unity[i]` == w^{-i}. Unusually, both
+     * `expanded_roots_of_unity` and `reverse_roots_of_unity` have length
+     * `max_width + 1`. By the above, `expanded_roots_of_unity[max_width] ==
+     * expanded_roots_of_unity[0] == 1` and similarly for
+     * `reverse_roots_of_unity`. The redundant element is just there to simplify
+     * index calculations in some formulas.
+     */
+
+    /** Ascending powers of the root of unity, length `max_width + 1`. */
+    pub expanded_roots_of_unity: *mut fr_t,
+    /** Descending powers of the root of unity, length `max_width + 1`. */
+    pub reverse_roots_of_unity: *mut fr_t,
+    /** Powers of the root of unity in bit-reversal permutation order, length
+     * `max_width`. */
+    pub roots_of_unity: *mut fr_t,
+
+    _marker: marker::PhantomData<&'a *mut fr_t>,
+}
+
+#[allow(non_camel_case_types)]
+#[repr(C)]
+pub struct KZGSettings<'a> {
+    /** The corresponding settings for performing FFTs. */
+    pub fs: *mut FFTSettings<'a>,
+    /** G1 group elements from the trusted setup,
+     * in Lagrange form bit-reversal permutation. */
+    pub g1_values: *mut g1_t,
+    /** G2 group elements from the trusted setup;
+     * both arrays have `FIELD_ELEMENTS_PER_BLOB` elements. */
+    pub g2_values: *mut g2_t,
+
+    _marker: marker::PhantomData<&'a *mut FFTSettings<'a>>,
+    _marker2: marker::PhantomData<&'a *mut g1_t>,
+    _marker3: marker::PhantomData<&'a *mut g2_t>,
+}
+
+pub fn blob_to_kzg_commitment(
+    out: *mut KZGCommitment,
+    blob: *const Blob,
+    s: *const KZGSettings,
+) -> C_KZG_RET {
+    todo!()
+}
+
+pub fn compute_kzg_proof(
+    proof_out: *mut KZGProof,
+    y_out: *mut Bytes32,
+    blob: *const Blob,
+    z_bytes: *const Bytes32,
+    s: *const KZGSettings,
+) -> C_KZG_RET {
+    todo!()
+}
+
+pub fn compute_blob_kzg_proof(
+    out: *mut KZGProof,
+    blob: *const Blob,
+    commitment_bytes: *const Bytes48,
+    s: *const KZGSettings,
+) -> C_KZG_RET {
+    todo!()
+}
+
+pub fn verify_kzg_proof(
+    ok: *mut bool,
+    commitment_bytes: *const Bytes48,
+    z_bytes: *const Bytes32,
+    y_bytes: *const Bytes32,
+    proof_bytes: *const Bytes48,
+    s: *const KZGSettings,
+) -> C_KZG_RET {
+    todo!()
+}
+
+pub fn verify_blob_kzg_proof(
+    ok: *mut bool,
+    blob: *const Blob,
+    commitment_bytes: *const Bytes48,
+    proof_bytes: *const Bytes48,
+    s: *const KZGSettings,
+) -> C_KZG_RET {
+    todo!()
+}
+
+pub fn verify_blob_kzg_proof_batch(
+    ok: *mut bool,
+    blobs: *const Blob,
+    commitments_bytes: *const Bytes48,
+    proofs_bytes: *const Bytes48,
+    n: usize,
+    s: *const KZGSettings,
+) -> C_KZG_RET {
+    todo!()
+}
