0e162b9099c14d68bc7328d7dbfea3f25514b4f4
Pablo Deymonnaz <deymonnaz@gmail.com>
2023-05-05 17:23:18 -0300

    polynomial_to_blob_with_size and polynomial_to_blob

--- src/utils.rs
+++ src/utils.rs
@@ -1,6 +1,7 @@
 use crate::commitments::kzg::{FrElement, StructuredReferenceString, G1};
 use crate::math::cyclic_group::IsGroup;
 use crate::math::elliptic_curve::traits::IsEllipticCurve;
+use crate::math::errors::ByteConversionError;
 use crate::math::unsigned_integer::element::U256;
 use crate::math::{
     elliptic_curve::{
@@ -12,23 +13,60 @@ use crate::math::{
     polynomial::Polynomial,
     traits::ByteConversion,
 };
-use crate::{BYTES_PER_BLOB, BYTES_PER_FIELD_ELEMENT};
+use crate::{BYTES_PER_BLOB, BYTES_PER_FIELD_ELEMENT, FE};
 use rand::Rng;
 
 pub fn blob_to_polynomial(
     input_blob: &[u8; BYTES_PER_BLOB],
-) -> Result<Polynomial<FrElement>, crate::math::errors::ByteConversionError>
+) -> Result<Polynomial<FrElement>, ByteConversionError>
 where
     FrElement: ByteConversion,
 {
     let mut coefficients = Vec::new();
 
     for elem_bytes in input_blob.chunks(BYTES_PER_FIELD_ELEMENT) {
-        let f = FrElement::from_bytes_le(elem_bytes)?;
+        let f = FrElement::from_bytes_be(elem_bytes)?;
         coefficients.push(f);
     }
 
     Ok(Polynomial::new(&coefficients))
+}
+
+pub fn polynomial_to_blob(polynomial: &Polynomial<FrElement>) -> Vec<u8>
+where
+    FrElement: ByteConversion,
+{
+    let coefficients = polynomial.coefficients();
+
+    coefficients
+        .iter()
+        .flat_map(|coef| coef.to_bytes_be())
+        .collect()
+}
+
+pub fn polynomial_to_blob_with_size(
+    polynomial: &Polynomial<FrElement>,
+) -> Result<[u8; BYTES_PER_BLOB], Vec<u8>>
+where
+    FrElement: ByteConversion,
+{
+    let coefficients = polynomial.coefficients();
+    let mut ret_vec: Vec<u8> = coefficients
+        .iter()
+        .flat_map(|coef| coef.to_bytes_be())
+        .collect();
+
+    let len = ret_vec.len();
+    let remaining = (BYTES_PER_BLOB - len) / BYTES_PER_FIELD_ELEMENT;
+
+    // pad with zeros until BYTES_PER_BLOB
+    let zero = FE::zero();
+    let zero_bytes = zero.to_bytes_be();
+
+    for _i in 0..remaining {
+        ret_vec.extend_from_slice(&zero_bytes);
+    }
+    ret_vec.try_into()
 }
 
 /// Helper function to create SRS. Once the deserialization of
@@ -56,4 +94,26 @@ pub fn create_srs() -> StructuredReferenceString<
         g2.operate_with_self(toxic_waste.representative()),
     ];
     StructuredReferenceString::new(&powers_main_group, &powers_secondary_group)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{blob_to_polynomial, polynomial_to_blob_with_size};
+    use crate::commitments::kzg::FrElement;
+    use crate::math::field::element::FieldElement;
+    use crate::math::polynomial::Polynomial;
+    use crate::FE;
+
+    #[test]
+    fn test_poly_to_blob_and_viceversa() {
+        let polynomial = Polynomial::<FrElement>::new(&[FieldElement::one()]);
+        let blob = polynomial_to_blob_with_size(&polynomial).unwrap();
+        let poly_from_blob = blob_to_polynomial(&blob).unwrap();
+
+        let one = FE::from(1);
+        let y_1 = polynomial.evaluate(&one);
+        let y_2 = poly_from_blob.evaluate(&one);
+
+        assert_eq!(y_1, y_2);
+    }
 }
