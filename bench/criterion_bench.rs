5c3d7e24b4d4f9a42737d06e0835a59d76a3bb52
Mario Rugiero <mrugiero@gmail.com>
2023-05-16 20:16:48 -0300

    Fix benchmark

--- bench/criterion_bench.rs
+++ bench/criterion_bench.rs
@@ -6,7 +6,7 @@ use lambdaworks_kzg::{
 fn bench_read_srs(c: &mut Criterion) {
     const TRUSTED_SETUP_FILE: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/test/trusted_setup.txt");
 
-    let s = load_trusted_setup_file(TRUSTED_SETUP_FILE).unwrap();
+    let s = load_trusted_setup_file(TRUSTED_SETUP_FILE.lines()).unwrap();
     c.bench_function("kzgsettings_to_ref_string", |b| {
         b.iter(|| black_box(kzgsettings_to_structured_reference_string(black_box(&s))));
     });
