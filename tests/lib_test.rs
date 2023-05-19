62204f7d78154114c7af2356ffd6b28c248d0046
Pablo Deymonnaz <pdeymon@fi.uba.ar>
2023-05-19 16:59:28 -0300

    Test batch proof (#40)
    * remove ignore test
    
    * test_compute_kzg_proof_for_a_simple_poly_2()
    
    * - integration tests moved to tests/ folder
    - pretty_assertions added
    
    * comment removed
    
    * test_compute_kzg_proof_for_a_simple_poly_2 corrected: poof and commitment is checked after calling verify_kzg_proof
    
    * fix verify_kzg_proof: using srs provided instead of default one
    
    * test_compute_kzg_proof_for_a_simple_poly_2 fixed. assert verify proof is true
    
    * clippy
    
    * unused part of test removed
    
    * clippy
    
    * initial test
    
    * merge
    
    * test_batch_proof
    
    * using msm in g1_lincomb
    
    * assert verify_blob_kzg_proof_batch is ok
    
    * Fixing verify_kzg_proof_batch 😀😀😀
    
    * comment changed in msm.
    
    * read file relative to CARGO_MANIFEST_DIR
    
    ---------
    
    Co-authored-by: Pablo Deymonnaz <deymonnaz@gmail.com>

--- tests/lib_test.rs
+++ tests/lib_test.rs
@@ -11,8 +11,8 @@ use lambdaworks_kzg::srs::{
 use lambdaworks_kzg::utils::polynomial_to_blob_with_size;
 use lambdaworks_kzg::{
     blst_p1, compute_kzg_proof, free_trusted_setup, kzgsettings_to_structured_reference_string,
-    verify_kzg_proof, Blob, Bytes32, Bytes48, FrElement, G1Point, KZGProof, KZGSettings, C_KZG_RET,
-    FE,
+    verify_blob_kzg_proof_batch, verify_kzg_proof, Blob, Bytes32, Bytes48, FrElement, G1Point,
+    KZGProof, KZGSettings, BYTES_PER_BLOB, C_KZG_RET, FE,
 };
 use pretty_assertions::assert_eq;
 
@@ -36,7 +36,10 @@ fn test_compute_kzg_proof_for_a_simple_poly() {
     let z_bytes: Bytes32 = FE::from(1).to_bytes_be().try_into().unwrap();
 
     // read srs from file
-    let lines = std::fs::read_to_string("tests/trusted_setup.txt").unwrap();
+    let lines = include_str!(concat!(
+        env!("CARGO_MANIFEST_DIR"),
+        "/tests/trusted_setup.txt"
+    ));
     let lines = lines.lines();
     let mut s = load_trusted_setup_file(lines).unwrap();
 
@@ -104,7 +107,10 @@ fn test_compute_kzg_proof_for_a_simple_poly_2() {
     let z_fr = FrElement::from_bytes_be(&z_bytes).unwrap();
 
     // read srs from file
-    let lines = std::fs::read_to_string("tests/trusted_setup.txt").unwrap();
+    let lines = include_str!(concat!(
+        env!("CARGO_MANIFEST_DIR"),
+        "/tests/trusted_setup.txt"
+    ));
     let lines = lines.lines();
     let mut s = load_trusted_setup_file(lines).unwrap();
 
@@ -161,8 +167,104 @@ fn test_compute_kzg_proof_for_a_simple_poly_2() {
 }
 
 #[test]
+fn test_batch_proof() {
+    let ok_enum_kzg = C_KZG_RET::C_KZG_OK;
+
+    // read srs from file
+    let lines = include_str!(concat!(
+        env!("CARGO_MANIFEST_DIR"),
+        "/tests/trusted_setup.txt"
+    ));
+    let lines = lines.lines();
+    let s = load_trusted_setup_file(lines).unwrap();
+
+    // assign poly 1
+    let polynomial = Polynomial::<FrElement>::new(&[FieldElement::one()]);
+    let blob = polynomial_to_blob_with_size(&polynomial).unwrap();
+    // z = 1
+    let z_bytes: Bytes32 = FE::from(1).to_bytes_be().try_into().unwrap();
+
+    // assign poly2 = x
+    let polynomial2 = Polynomial::<FrElement>::new(&[FieldElement::zero(), FieldElement::one()]);
+    let blob2 = polynomial_to_blob_with_size(&polynomial2).unwrap();
+    // z = 2
+    let z_bytes2: Bytes32 = FE::from(2).to_bytes_be().try_into().unwrap();
+    let mut blob_both: [u8; BYTES_PER_BLOB * 2] = [0; BYTES_PER_BLOB * 2];
+    blob_both[..BYTES_PER_BLOB].copy_from_slice(&blob);
+    blob_both[BYTES_PER_BLOB..].copy_from_slice(&blob2);
+
+    // output buffers 1
+    let mut proof_out1: KZGProof = [0; 48];
+    let mut y_out1: Bytes32 = [0; 32];
+
+    // output buffers 2
+    let mut proof_out2: KZGProof = [0; 48];
+    let mut y_out2: Bytes32 = [0; 32];
+
+    let ret = compute_kzg_proof(
+        &mut proof_out1 as *mut KZGProof,
+        &mut y_out1 as *mut Bytes32,
+        &blob as *const Blob,
+        &z_bytes as *const Bytes32,
+        &s as *const KZGSettings,
+    );
+
+    // assert ret function
+    assert_eq!(ret, ok_enum_kzg);
+
+    let ret = compute_kzg_proof(
+        &mut proof_out2 as *mut KZGProof,
+        &mut y_out2 as *mut Bytes32,
+        &blob2 as *const Blob,
+        &z_bytes2 as *const Bytes32,
+        &s as *const KZGSettings,
+    );
+
+    // assert ret function
+    assert_eq!(ret, ok_enum_kzg);
+
+    // build proof_batch
+    let mut proof_out_batch = [0_u8; 96];
+    proof_out_batch[..48].copy_from_slice(&proof_out1);
+    proof_out_batch[48..].copy_from_slice(&proof_out2);
+
+    // build y_out_batch
+    let mut y_out_batch = [0_u8; 64];
+    y_out_batch[..32].copy_from_slice(&y_out1);
+    y_out_batch[32..].copy_from_slice(&y_out2);
+
+    let srs = kzgsettings_to_structured_reference_string(&s).unwrap();
+    let kzg = lambdaworks_kzg::KZG::new(srs);
+    let commitment1 = kzg.commit(&polynomial);
+    let commitment_bytes1 = compress_g1_point(&commitment1).unwrap();
+
+    let commitment2 = kzg.commit(&polynomial2);
+    let commitment_bytes2 = compress_g1_point(&commitment2).unwrap();
+    let mut commitment_bytes_batch = [0_u8; 96];
+    commitment_bytes_batch[..48].copy_from_slice(&commitment_bytes1);
+    commitment_bytes_batch[48..].copy_from_slice(&commitment_bytes2);
+
+    // verify blob as a batch
+    let mut ok = false;
+    verify_blob_kzg_proof_batch(
+        &mut ok as *mut bool,
+        blob_both.as_ptr() as *const Blob,
+        commitment_bytes_batch.as_ptr() as *const Bytes48,
+        proof_out_batch.as_ptr() as *const Bytes48,
+        2,
+        &s as *const KZGSettings,
+    );
+
+    assert!(ok);
+    assert_eq!(ret, ok_enum_kzg);
+}
+
+#[test]
 fn test_read_srs() {
-    let lines = std::fs::read_to_string("tests/trusted_setup.txt").unwrap();
+    let lines = include_str!(concat!(
+        env!("CARGO_MANIFEST_DIR"),
+        "/tests/trusted_setup.txt"
+    ));
     let lines = lines.lines();
     let (g1_points, g2_points) = load_trusted_setup_file_to_g1_points_and_g2_points(lines).unwrap();
 
@@ -174,7 +276,10 @@ fn test_read_srs() {
             "97f1d3a73197d7942695638c4fa9ac0fc3688c4f9774b905a14e3a3f171bac586c55e83ff97a1aeffb3af00adb22c6bb");
 
     let srs_from_file = vecs_to_structured_reference_string(&g1_points, &g2_points);
-    let lines = std::fs::read_to_string("tests/trusted_setup.txt").unwrap();
+    let lines = include_str!(concat!(
+        env!("CARGO_MANIFEST_DIR"),
+        "/tests/trusted_setup.txt"
+    ));
     let lines = lines.lines();
     let mut s = load_trusted_setup_file(lines).unwrap();
     let srs = kzgsettings_to_structured_reference_string(&s).unwrap();
